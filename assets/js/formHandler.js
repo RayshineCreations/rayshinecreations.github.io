7aafc9b3a6f749593fb5913250834e245f4db621
AdityaThadeshwar <aditya.thadeshwar@gmail.com>
2024-03-17 15:22:39 +0530

    Updated form

--- assets/js/formHandler.js
+++ assets/js/formHandler.js
@@ -1,36 +1,10 @@
-// Initialize Firebase
-var firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  
-  // Reference to your Firebase Realtime Database
-  var database = firebase.database();
-  
-  // Function to handle form submission
-  function submitForm() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    // Add more fields as needed
-    
-    // Push data to Firebase
-    database.ref('formSubmissions').push({
-      name: name,
-      email: email
-      // Add more fields as needed
-    }).then(function() {
-      // Handle successful submission
-      console.log('Form submitted successfully');
-    }).catch(function(error) {
-      // Handle errors
-      console.error('Error submitting form:', error);
-    });
-  }
-  
+const form = document.querySelector('.gform');
+const successMessage = document.querySelector('.success_text');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    successMessage.classList.add('show');
+    setTimeout(() => form.submit(), 2000);
+} );
+
+
+//https://script.google.com/macros/s/AKfycbzib4ucM0Q_euLKiEPurGoGU-pl31Dm8HBsr08ObTDqEcWWUf-Qf3SWb5ryQAnXJBFQ/exec
